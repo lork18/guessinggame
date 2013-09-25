96e92e86c9853838c625e367ab2bfe2e372eb97f
Kia Lor <lork18@uww.edu>
2013-09-25 17:00:08 -0500

    Added display of number of attempts

--- GuessingGame.java
+++ GuessingGame.java
@@ -14,18 +14,23 @@ public class GuessingGame {
 	
 	public static void main (String[] args){
                 int number = generateRandomInt();
+		int triesCount = 0;
 		while(true){
 			System.out.println("Enter your guess: ");
 			int guess = in.nextInt();	
 			if(guess > number){
 				System.out.println("Too high.");
+				triesCount = triesCount + 1;
 			}
 			else if(guess < number){
 				System.out.println("Too low.");
+				triesCount = triesCount + 1;
 			}
 			else{
+				triesCount = triesCount + 1;
 				System.out.println("You got it!");
 				System.out.println("The number was " + number);
+				System.out.println("Number of tries: " + triesCount);
 				break;
 			}
 		}
